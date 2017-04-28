5baeaa3fe207a144bb08035a1d9b020082b55809
Emrin <emrin95@gmail.com>
2017-04-28 19:14:05 +0200

    Update prg.js

--- prg.js
+++ prg.js
@@ -3,11 +3,12 @@ xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var myObj = this.responseText;
         var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
-        document.getElementById("demo").innerHTML = jsonPretty;
+        document.getElementById("demo5").innerHTML = jsonPretty;
     }
 };
 xmlhttp.open("GET", "https://api.blockcypher.com/v1/btc/main", true);
 xmlhttp.send();
+
 
 //WS /blockchaininfo
 var xmlhttp = new XMLHttpRequest();
@@ -15,24 +16,40 @@ xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var myObj = this.responseText;
         var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
-        document.getElementById("demo2").innerHTML = jsonPretty;
+        document.getElementById("demo4").innerHTML = jsonPretty;
     }
 };
 xmlhttp.open("GET", "https://api.blockcypher.com/v1/btc/main/blocks/294322?txstart=1&limit=1", true);
 xmlhttp.send();
 
+
+
+//WS /blockchaininfo
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var myObj = this.responseText;
+        var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
+        document.getElementById("demo3").innerHTML = jsonPretty;
+    }
+};
+xmlhttp.open("GET", "https://api.blockcypher.com/v1/btc/main/blocks/294322?txstart=1&limit=1", true);
+xmlhttp.send();
+
+
+
 function startSearch() {
-    //var request = document.getElementById("search").value;
+    var url = document.getElementById("search").value;
     
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             var myObj = this.responseText;
             var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
-            document.getElementById("demo5").innerHTML = jsonPretty;
+            document.getElementById("demo").innerHTML = jsonPretty;
         }
     };
-    xmlhttp.open("GET", "https://api.blockcypher.com/v1/btc/main/blocks/294322?txstart=1&limit=1", true);
+    xmlhttp.open("GET", url, true);
     xmlhttp.send();
 }
 
