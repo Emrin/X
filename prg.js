d5e8b9ae579a93cf986c9caf314d90dd374b875a
Emrin <emrin95@gmail.com>
2017-04-28 18:57:40 +0200

    Update prg.js

--- prg.js
+++ prg.js
@@ -32,7 +32,7 @@ function startSearch() {
             document.getElementById("demo5").innerHTML = jsonPretty;
         }
     };
-    xmlhttp.open("GET", "https://api.blockcypher.com/v1/btc/main", true);
+    xmlhttp.open("GET", "https://api.blockcypher.com/v1/btc/main/blocks/294322?txstart=1&limit=1", true);
     xmlhttp.send();
 }
 
