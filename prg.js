a1a89c268d802a0856383e904064deb06e9facdc
Emrin <emrin95@gmail.com>
2017-04-28 17:39:51 +0200

    Update prg.js

--- prg.js
+++ prg.js
@@ -1,3 +1,15 @@
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var myObj = this.responseText;
+        var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
+        document.getElementById("demo").innerHTML = jsonPretty;
+    }
+};
+xmlhttp.open("GET", "https://api.blockcypher.com/v1/btc/main", true);
+xmlhttp.send();
+
+
 function startSearch()
 {
     var request = document.getElementById("search").value;
@@ -7,7 +19,7 @@ function startSearch()
         if (this.readyState == 4 && this.status == 200) {
             var myObj = this.responseText;
             var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
-            document.getElementById("demo").innerHTML = jsonPretty;
+            document.getElementById("demo2").innerHTML = jsonPretty;
         }
     };
     xmlhttp.open("GET", "https://api.blockcypher.com/v1/btc/main", true);
