93c94574da6621ce049d15e2fe0614d2190c6274
Emrin <emrin95@gmail.com>
2017-04-28 17:30:46 +0200

    Update prg.js

--- prg.js
+++ prg.js
@@ -1,6 +1,7 @@
 function startSearch()
 {
     var request = document.getElementById("search").value;
+    
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
