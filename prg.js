5d13cbc8ec86fe744a8259f3dc88a5f8cf5b624c
Emrin <emrin95@gmail.com>
2017-04-28 17:55:56 +0200

    Update prg.js

--- prg.js
+++ prg.js
@@ -19,7 +19,7 @@ function startSearch()
         if (this.readyState == 4 && this.status == 200) {
             var myObj = this.responseText;
             var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
-            document.getElementById("demo2").innerHTML = jsonPretty;
+            document.getElementById("demo").innerHTML = jsonPretty;
         }
     };
     xmlhttp.open("GET", "https://api.blockcypher.com/v1/btc/main", true);
